2eb43619238843a4885254a104f7f0bf3f9cae41
wookjongkim <121083077+wookjongkim@users.noreply.github.com>
2023-05-21 10:19:47 +0900

    first commit

---
+++ src/main/java/zerobase/weatherprojectstudy/WeatherProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package zerobase.weatherprojectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProjectStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherProjectStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/weatherprojectstudy/WeatherProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.weatherprojectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherProjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
